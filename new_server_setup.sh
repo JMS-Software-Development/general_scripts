264aca1604e718715a24d5cc835ad1467bb29eeb
Jonathan Raes <jonathan@medianasoftware.nl>
2022-02-16 16:05:56 +0100

    variable username, postgres setup, many fixes

--- new_server_setup.sh
+++ new_server_setup.sh
@@ -4,32 +4,44 @@ SERVER_NAME=${SERVER_NAME:-medianasoftware.nl}
 read -p "Enter project name: " PROJECT_NAME
 PROJECT_NAME=${PROJECT_NAME:jad}
 
-read -p "Should this be a postgres server? (y/n): " SETUP_POSTGRES
+read -p "Enter username: " USER_NAME
+USER_NAME=${USER_NAME:mediana}
+
+read -p "Should this be a postgres server? (y/N): " SETUP_POSTGRES
 SETUP_POSTGRES=${SETUP_POSTGRES:-n}
 
-read -p "Setup cerbot for HTTPS? (y/n): " SETUP_CERTBOT
+read -p "Setup cerbot for HTTPS? (y/N): " SETUP_CERTBOT
 SETUP_CERTBOT=${SETUP_CERTBOT:-n}
 
-# add project name to environment
+read -p "Setup cerbot nodejs+npm? (y/N): " SETUP_NODE
+SETUP_NODE=${SETUP_NODE:-n}
+
+# add project + required settings name to environment
 echo PROJECT_NAME=$PROJECT_NAME >> /etc/environment
+echo USE_NODE=$SETUP_NODE >> /etc/environment
 
 apt update
-apt install -y nginx python3-venv python3-wheel gcc python3-dev redis-server
+apt install -y nginx python3-venv python3-wheel gcc python3-dev redis-server 
 
-# Setup mediana user
-adduser --disabled-password --gecos "" mediana
-echo 'mediana ALL=(ALL) NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
-usermod -aG sudo mediana
+if [ $SETUP_NODE = "y" ]
+then
+    apt install -y nodejs npm
+fi
 
-mkdir /home/mediana/.ssh
-cp /root/.ssh/authorized_keys /home/mediana/.ssh/authorized_keys
-chown -R mediana:mediana /home/mediana/.ssh
+# Setup new user
+adduser --disabled-password --gecos "" $USER_NAME
+echo "$USER_NAME ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
+usermod -aG sudo $USER_NAME
+
+mkdir /home/$USER_NAME/.ssh
+cp /root/.ssh/authorized_keys /home/$USER_NAME/.ssh/authorized_keys
+chown -R $USER_NAME:$USER_NAME /home/$USER_NAME/.ssh
 systemctl restart sshd
 
 # Setup project dirs and config 
-mkdir -p /home/mediana/$PROJECT_NAME/$PROJECT_NAME
-mkdir -p /home/mediana/$PROJECT_NAME-logs/
-mkdir -p /home/mediana/database-backups/
+mkdir -p /home/$USER_NAME/$PROJECT_NAME/$PROJECT_NAME
+mkdir -p /home/$USER_NAME/$PROJECT_NAME-logs/
+mkdir -p /home/$USER_NAME/database-backups/
 
 # Setup Firewall
 apt install -y ufw
@@ -46,9 +58,13 @@ import os
 ALLOWED_HOSTS = ['$SERVER_NAME']
 DEBUG = False
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'asjklfhaskldjfhasjklfhklasjdhf')
-STATIC_ROOT = \"/var/www/$PROJECT_NAME/static\"
 
-LOG_BASE_PATH = \"/home/mediana/$PROJECT_NAME-logs/\"
+STATIC_ROOT = \"/var/www/$PROJECT_NAME/static\"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, \"static\"),
+]
+
+LOG_BASE_PATH = \"/home/$USER_NAME/$PROJECT_NAME-logs/\"
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -83,51 +99,53 @@ LOGGING = {
     },
 }
 
-" > /home/mediana/$PROJECT_NAME/$PROJECT_NAME/production.py
+" > /home/$USER_NAME/$PROJECT_NAME/$PROJECT_NAME/production.py
 
 cd ~
-python3 -m venv /home/mediana/venv
-source /home/mediana/venv/bin/activate
+python3 -m venv /home/$USER_NAME/venv
+source /home/$USER_NAME/venv/bin/activate
 pip3 install wheel daphne django
 
 # Create repository
-mkdir -p /home/mediana/$PROJECT_NAME.git
-cd /home/mediana/$PROJECT_NAME.git
+mkdir -p /home/$USER_NAME/$PROJECT_NAME.git
+cd /home/$USER_NAME/$PROJECT_NAME.git
 git init --bare
 
 # Create django secret key
-DJANGO_SECRET=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
+DJANGO_KEY=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
 
 # Create hook
 echo "Creating deployment hook"
-cd /home/mediana/$PROJECT_NAME.git/hooks
-wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/post-receive -O /home/mediana/$PROJECT_NAME.git/hooks/post-receive
+cd /home/$USER_NAME/$PROJECT_NAME.git/hooks
+wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/post-receive -O /home/$USER_NAME/$PROJECT_NAME.git/hooks/post-receive
+sed -i "s/\$USER_NAME/$USER_NAME/g" /home/$USER_NAME/$PROJECT_NAME.git/hooks/post-receive
 chmod +x post-receive
-chown -R mediana:mediana /home/mediana/
+chown -R $USER_NAME:$USER_NAME /home/$USER_NAME/
 
 # Create services
 echo "Creating daphne service"
-wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/daphne.service -O /etc/systemd/system/daphne.service
-sed -i "s/\$DJANGO_SECRET/$DJANGO_SECRET/g" /etc/systemd/system/daphne.service
-sed -i "s/\$PROJECT_NAME/$PROJECT_NAME/g" /etc/systemd/system/daphne.service
-sed -i "s/\$SERVER_NAME/$SERVER_NAME/g" /etc/systemd/system/daphne.service
+wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/django-daphne.service -O /etc/systemd/system/daphne.service
+sed -i "s/\$DJANGO_SECRET/$DJANGO_KEY/g" /etc/systemd/system/django-daphne.service
+sed -i "s/\$PROJECT_NAME/$PROJECT_NAME/g" /etc/systemd/system/django-daphne.service
+sed -i "s/\$SERVER_NAME/$SERVER_NAME/g" /etc/systemd/system/django-daphne.service
 echo "Created daphne service"
 echo 
 
-mkdir -p /etc/conf.d/
-mkdir -p /var/log/celery
-mkdir -p /var/log/celerybeat
-chown mediana:mediana /var/log/celery
-chown mediana:mediana /var/log/celerybeat
-echo "Creating celery service"
-wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/celery.service -O /etc/systemd/system/celery.service
-wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/celery -O /etc/conf.d/celery
-echo 
+# Celery and celerybeat
+# mkdir -p /etc/conf.d/
+# mkdir -p /var/log/celery
+# mkdir -p /var/log/celerybeat
+# chown $USER_NAME:$USER_NAME /var/log/celery
+# chown $USER_NAME:$USER_NAME /var/log/celerybeat
+# echo "Creating celery service"
+# wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/celery.service -O /etc/systemd/system/celery.service
+# wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/celery -O /etc/conf.d/celery
+# echo 
 
-echo "Creating celerybeat service"
-wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/celerybeat.service -O /etc/systemd/system/celerybeat.service
-wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/celerybeat -O /etc/conf.d/celerybeat
-echo 
+# echo "Creating celerybeat service"
+# wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/celerybeat.service -O /etc/systemd/system/celerybeat.service
+# wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/celerybeat -O /etc/conf.d/celerybeat
+# echo 
 
 # Set up nginx
 echo "Setting up nginx"
@@ -143,34 +161,31 @@ systemctl restart nginx
 
 mkdir -p "/var/www/$PROJECT_NAME/media/"
 mkdir -p "/var/www/$PROJECT_NAME/static/"
-chown -R mediana:mediana "/var/www/$PROJECT_NAME/static/"
-chown -R mediana:mediana "/var/www/$PROJECT_NAME/static/"
-
-# Unused directory?
-mkdir -p "/var/www/static"
-chown -R mediana:mediana "/var/www/static/"
+chown -R $USER_NAME:$USER_NAME "/var/www/$PROJECT_NAME/static/"
+chown -R $USER_NAME:$USER_NAME "/var/www/$PROJECT_NAME/static/"
 
 if [ $SETUP_POSTGRES = "y" ]
 then
     echo "Installing postgres..."
-    wget https://github.com/MedianaSoftware/general_scripts/blob/master/setup_postgres.sh -O /home/mediana/setup_postgres.sh
-    bash /home/mediana/setup_postgres.sh
+    wget https://github.com/MedianaSoftware/general_scripts/blob/master/setup_postgres.sh -O /home/$USER_NAME/setup_postgres.sh
+    chmod +x /home/$USER_NAME/setup_postgres.sh
+    POSTGRES_USER="$PROJECT_NAME"_user POSTGRES_DB="$PROJECT_NAME" . /home/$USER_NAME/setup_postgres.sh
 fi
 
 if [ $SETUP_CERTBOT = "y" ]
 then
     echo "Setting up certbot..."
-    wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/setup_certbot.sh -O /home/mediana/setup_certbot.sh
-    bash /home/mediana/setup_certbot.sh
+    wget https://raw.githubusercontent.com/MedianaSoftware/general_scripts/master/setup_certbot.sh -O /home/$USER_NAME/setup_certbot.sh
+    bash /home/$USER_NAME/setup_certbot.sh
 fi
 
 systemctl daemon-reload
-systemctl enable daphne
-systemctl enable celery
-systemctl enable celerybeat
+systemctl enable django-daphne
+# systemctl enable celery
+# systemctl enable celerybeat
 systemctl enable redis-server
 
-chown -R mediana:mediana /home/mediana/$PROJECT_NAME
+chown -R $USER_NAME:$USER_NAME /home/$USER_NAME/$PROJECT_NAME
 
 echo "Dont forget to add the following to settings.py:"
 echo """
@@ -189,5 +204,5 @@ except ImportError:
 # Receive code
 echo ""
 echo "Then, please run the following command:"
-echo "git remote add deploy ssh://mediana@$SERVER_NAME/home/mediana/$PROJECT_NAME.git/ && git push deploy"
+echo "git remote add deploy ssh://$USER_NAME@$SERVER_NAME/home/$USER_NAME/$PROJECT_NAME.git/ && git push deploy"
 echo "in your local repository."
--- setup_postgres.sh
+++ setup_postgres.sh
@@ -2,24 +2,22 @@ sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib
 sudo apt-get install libpq-dev python3-dev
 pip install psycopg2
-sudo -u postgres psql
 
-# CREATE DATABASE mydb;
-# CREATE USER myuser WITH ENCRYPTED PASSWORD 'mypass';
+PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-24};echo;)
+sudo -u postgres createuser $POSTGRES_USER
+sudo -u postgres createdb $POSTGRES_DB
+sudo -u postgres psql -c "ALTER USER $POSTGRES_USER WITH ENCRYPTED PASSWORD $PASSWORD;"
+sudo -u postgres psql -c "GRANT ALL privileges on database $POSTGRES_DB to $POSTGRES_USER;"
 
-# ALTER ROLE myuser SET client_encoding TO 'utf8';
-# ALTER ROLE myuser SET default_transaction_isolation TO 'read committed';
-# ALTER ROLE myuser SET timezone TO 'UTC';
-
-# GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'mydb',
-#        'USER': 'myuser',
-#        'PASSWORD': 'mypass',
-#        'HOST': 'localhost',
-#        'PORT': '',
-#    }
-#}
+echo """
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "$POSTGRES_DB",
+        'USER': "$POSTGRES_USER",
+        'PASSWORD': "$PASSWORD",
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+""" >> /home/$USER_NAME/$PROJECT_NAME/$PROJECT_NAME/production.py
